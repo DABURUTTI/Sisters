693ec10fa2452db49dd49014b88ce73885cc660b
DABURUTTI <daburutti@gmail.com>
2019-01-16 16:03:15 +0900

    静的なファイルを送信する

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
+//@ts-check
+
 "use strict";
 
 const http = require('http');
 const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
+
+const HTML_DIR_PATH = '/html/index.html';
 
 var progressnum = 0;
 
@@ -9,26 +14,64 @@ let server = http.createServer(httpServer);
 server.listen('3030');
 
 function httpServer(req, res) {
-    if(req.url == '/favicon.ico') {
+    if (req.url == '/favicon.ico') {
         res.end();
         return;
     }
 
-    if(req.url == '/start_encode') {
+    if (req.url == '/start_encode') {
         res.write("encodeStarting...");
 
         start_encode();
         res.end("starting");
-        return;
+        return
     }
 
-    res.write(progressnum.toString());
-    res.end();
+    if (req.url == '/progress') {
+        res.write(progressnum.toString());
+        res.end();
+        return
+    }
 
-    return;
+    //静的ファイルの送信
+
+    console.log(req.url);
+    console.log(__dirname + HTML_DIR_PATH);
+    if (req.url == "/") {
+
+        fs.readFile (__dirname + HTML_DIR_PATH, 'UTF-8', function (err, data) {
+            if(err) {
+
+                console.log(err);
+                res.writeHead(404, {'Content-Type': 'text/plain'});
+                res.write('notfound!!');
+                return res.end();
+
+            }else{
+                res.writeHead(200, {'Contetnt-Type': 'text/html'});
+                res.write(data);
+                return res.end();
+        }})
+    } else {
+        fs.readFile (__dirname + req.url, 'UTF-8', function (err, data) {
+            if(err) {
+                res.writeHead(404, {'Content-Type': 'text/plain'});
+                res.write('notfound!!');
+                return res.end();
+
+            }else{
+                res.writeHead(200, {'Contetnt-Type': 'text/html'});
+                res.write(data);
+                return res.end();
+        }})
+    }
 }
 
-function start_encode() {
+function Setup_EncodeOption () {
+
+}
+
+function start_encode () {
     ffmpeg('/path/to/file.avi')
         .videoCodec('libx264')
         .audioCodec('libmp3lame')
