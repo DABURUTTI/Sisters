2625f338a866d077dc77fe48e2f29ea7e75cfe8f
DABURUTTI <daburutti@gmail.com>
2019-01-17 21:12:30 +0900

    ヘッダーを追加するように変更　使用するBootstrapをHONOKAに変更

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ const http = require('http');
 const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
 
-const HTML_DIR_PATH = '/html/index.html';
+const PATH_WEB_INDEX = '/html/index.html';
+const PATH_WEB_DIR = '/html';
 
 var progressnum = 0;
 
@@ -35,11 +36,10 @@ function httpServer(req, res) {
 
     //静的ファイルの送信
 
-    console.log(req.url);
-    console.log(__dirname + HTML_DIR_PATH);
+    console.log(__dirname + PATH_WEB_DIR + req.url);
     if (req.url == "/" || req.url == "/index.html") {
 
-        fs.readFile (__dirname + HTML_DIR_PATH, 'UTF-8', function (err, data) {
+        fs.readFile (__dirname + PATH_WEB_INDEX, 'UTF-8', function (err, data) {
             if(err) {
 
                 console.log(err);
@@ -53,14 +53,34 @@ function httpServer(req, res) {
                 return res.end();
         }})
     } else {
-        fs.readFile (__dirname + req.url, 'UTF-8', function (err, data) {
+        fs.readFile (__dirname + PATH_WEB_DIR + req.url, 'UTF-8', function (err, data) {
             if(err) {
                 res.writeHead(404, {'Content-Type': 'text/plain'});
                 res.write('notfound!!');
-                return res.end();
+                res.end();
+                return
 
             }else{
-                res.writeHead(200, {'Contetnt-Type': 'text/html'});
+
+                let ext = req.url.split('.').pop();
+
+                console.log("Type:" + ext );
+                switch(ext.toString()) {
+
+                    case "png":
+
+                    res.writeHead(200, {'Contetnt-Type': 'image/png'});
+
+                    case "jpg":
+                    case "jpeg":
+
+                    res.writeHead(200, {'Contetnt-Type': 'image/jpeg'});
+
+                    default:
+                    res.writeHead(200, {'Contetnt-Type': 'text/html'});
+
+                }
+
                 res.write(data);
                 return res.end();
         }})
